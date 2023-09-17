555b2bfca77166d87f472e3f58637d7e9798263c
Tran Nhat Duy <42115291+hitonichi@users.noreply.github.com>
2023-09-17 16:01:55 +0700

    feat: add theming capabilities

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,23 +1,37 @@
-import './globals.css'
-import type { Metadata } from 'next'
-import { ReduxProvider } from '@/store/provider'
-
+import './globals.css';
+import type { Metadata } from 'next';
+import { ReduxProvider } from '@/store/provider';
+import { ConfigProvider } from 'antd';
 
 export const metadata: Metadata = {
   title: 'Image Processing Tool',
-}
+};
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
       <body>
-        <ReduxProvider>{children}</ReduxProvider>
+        <ReduxProvider>
+          <ConfigProvider
+          // theme={{
+          //   token: {
+          //     // Seed Token
+          //     colorPrimary: '#00b96b',
+          //     borderRadius: 2,
+
+          //     // Alias Token
+          //     colorBgContainer: '#f6ffed',
+          //   },
+          // }}
+          >
+            {children}
+          </ConfigProvider>
+        </ReduxProvider>
       </body>
     </html>
-  )
+  );
 }
-
--- src/store/reducers/counterReducer.ts
+++ src/store/reducers/counterReducer.ts
@@ -1,34 +1,35 @@
 //Example template for reducers
-import { createSlice, PayloadAction } from '@reduxjs/toolkit'
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 interface CounterState {
-  value: number
+  value: number;
 }
 
 // Define the initial state using that type
 const initialState: CounterState = {
-  value: 0
-}
+  value: 0,
+};
 
 export const counterSlice = createSlice({
   name: 'counter',
   initialState,
   reducers: {
-    increment: state => {
-      state.value += 1
+    increment: (state) => {
+      state.value += 1;
     },
-    decrement: state => {
-      state.value -= 1
+    decrement: (state) => {
+      state.value -= 1;
     },
     incrementByAmount: (state, action: PayloadAction<number>) => {
-      state.value += action.payload
-    }
-  }
-})
+      state.value += action.payload;
+    },
+  },
+});
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions
+export const { increment, decrement, incrementByAmount } = counterSlice.actions;
 
 // Other code such as selectors can use the imported `rootState` type
-export const selectCount = (state: any) => state.counter.value
+// eslint-disable-next-line @typescript-eslint/no-explicit-any
+export const selectCount = (state: any) => state.counter.value;
 
-export default counterSlice.reducer
+export default counterSlice.reducer;
