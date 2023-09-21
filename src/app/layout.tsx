67fac5d0c173ecea82b32bec5848afcc72a6de5d
Tran Nhat Duy <42115291+hitonichi@users.noreply.github.com>
2023-09-22 02:11:59 +0700

    feat: update favicon to HCMUT logo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -14,6 +14,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/HCMUT_official_logo.ico"></link>
+      </head>
       <body>
         <ReduxProvider>
           <ConfigProvider
