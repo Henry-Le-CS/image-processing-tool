b85a709ce5b772adf9c2f5a6f9f1b7522c9e6981
Henry Le <lehieu2077@gmail.com>
2023-09-25 16:50:48 +0700

    feat: add new reducers for selecting image

--- src/store/reducers/imageListReducer.ts
+++ src/store/reducers/imageListReducer.ts
@@ -3,7 +3,7 @@ import { createSlice, PayloadAction } from '@reduxjs/toolkit';
 
 export interface IImageListState {
   data: {
-    [key:string]: IImageData;
+    [key: string]: IImageData;
   };
 }
 
@@ -29,13 +29,56 @@ export const imageListSlice = createSlice({
     resetOne: (state, action: PayloadAction<string>) => {
       state.data[action.payload].isModified = false;
     },
+    resetAll: (state) => {
+      for (const [fileId, imageData] of Object.entries(state.data)) {
+        state.data[fileId] = {
+          ...imageData,
+          isModified: false,
+          trafficCondition: {}
+        }
+      }
+    },
     setAll: (state, action: PayloadAction<IImageListState>) => {
       state.data = action.payload.data;
+    },
+    selectOne: (state, action: PayloadAction<{ fileId: string, isSelected: boolean }>) => {
+      const fileId = action.payload.fileId
+      const isSelected = action.payload.isSelected
+
+      state.data[fileId].isSelected = isSelected
+    },
+    selectAll: (state) => {
+      for (const [fileId, imageData] of Object.entries(state.data)) {
+        state.data[fileId] = {
+          ...imageData,
+          isSelected: true,
+        }
+      }
+    },
+    deselectAll: (state) => {
+      for (const [fileId, imageData] of Object.entries(state.data)) {
+        state.data[fileId] = {
+          ...imageData,
+          isSelected: false,
+        }
+      }
+    },
+    updateSelected: (state, action: PayloadAction<{ data: ITrafficData }>) => {
+      for (const [fileId, imageData] of Object.entries(state.data)) {
+        if (imageData.isSelected) {
+          state.data[fileId] = {
+            ...imageData,
+            isModified: true,
+            isSelected: false, // Deselect once we modified
+            trafficCondition: action.payload.data,
+          }
+        }
+      }
     },
   },
 });
 
-export const { updateOne, resetOne, setAll } = imageListSlice.actions;
+export const { updateOne, resetOne, setAll, resetAll, selectOne, selectAll, deselectAll, updateSelected } = imageListSlice.actions;
 
 // Other code such as selectors can use the imported `rootState` type
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
