44c518e3098a2aa8bee3c49f8db2dddd291de332
Tran Nhat Duy <42115291+hitonichi@users.noreply.github.com>
2023-09-25 21:11:18 +0700

    fix: ellipsis for img card title to prevent overlapping checkbox

--- src/components/ImageCard/components/CardTitle.tsx
+++ src/components/ImageCard/components/CardTitle.tsx
@@ -1,35 +1,48 @@
-import { IImageData } from "@/data/types";
-import { selectImageList, selectOne } from "@/store/reducers/imageListReducer";
-import { useAppDispatch, useAppSelector } from "@/store/store";
-import { FC, HTMLAttributes, useMemo } from "react";
+import { IImageData } from '@/data/types';
+import { selectImageList, selectOne } from '@/store/reducers/imageListReducer';
+import { useAppDispatch, useAppSelector } from '@/store/store';
+import { Typography } from 'antd';
+import { FC, HTMLAttributes, useMemo } from 'react';
 
 export interface IImageCardProps extends HTMLAttributes<HTMLDivElement> {
-    fileName: string;
-    fileId: string;
+  fileName: string;
+  fileId: string;
 }
 
 const CardTitle: FC<IImageCardProps> = ({ fileName, fileId }) => {
-    const images: {
-        [key: string]: IImageData;
-    } = useAppSelector(selectImageList);
+  const images: {
+    [key: string]: IImageData;
+  } = useAppSelector(selectImageList);
 
-    const isImageSelected = useMemo(() =>
-        images[fileId]?.isSelected || false
-        , [images, fileId])
+  const isImageSelected = useMemo(
+    () => images[fileId]?.isSelected || false,
+    [images, fileId]
+  );
 
-    const cardDispatcher = useAppDispatch()
+  const cardDispatcher = useAppDispatch();
 
-    const handleRadioCheck = () => {
-        cardDispatcher(selectOne({
-            fileId,
-            isSelected: !isImageSelected
-        }))
-    }
+  const handleRadioCheck = () => {
+    cardDispatcher(
+      selectOne({
+        fileId,
+        isSelected: !isImageSelected,
+      })
+    );
+  };
 
-    return <div className="flex items-center justify-between">
-        <p className="text-sm font-semibold">{fileName}</p>
-        <input type="checkbox" value={fileId} checked={isImageSelected} onChange={handleRadioCheck} />
+  return (
+    <div className="flex items-center justify-between">
+      <Typography.Text className="text-sm font-semibold" ellipsis>
+        {fileName}
+      </Typography.Text>
+      <input
+        type="checkbox"
+        value={fileId}
+        checked={isImageSelected}
+        onChange={handleRadioCheck}
+      />
     </div>
-}
+  );
+};
 
-export default CardTitle
+export default CardTitle;
