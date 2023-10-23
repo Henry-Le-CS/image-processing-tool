4b06f0dcf48fcc17863472d968bd95b723effdaf
Tran Nhat Duy <42115291+hitonichi@users.noreply.github.com>
2023-10-23 16:40:20 +0700

    refactor: move labelling to its designated route

---
+++ src/app/labelling/page.tsx
@@ -0,0 +1,11 @@
+'use client';
+
+import ImageGrid from '@/components/ImageGrid';
+
+export default function Home() {
+  return (
+    <div className="mb-4 w-full h-full flex flex-col justify-center items-center">
+      <ImageGrid></ImageGrid>
+    </div>
+  );
+}
