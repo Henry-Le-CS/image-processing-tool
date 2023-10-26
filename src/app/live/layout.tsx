517359981dbb976024d9fa20c5bf3f2e9cd045d7
Henry Le <lehieu2077@gmail.com>
2023-10-27 06:09:32 +0700

    dispatch: fix layout file

--- src/app/live/layout.tsx
+++ src/app/live/layout.tsx
@@ -1,10 +1,4 @@
-import { HTMLAttributes } from "react"
-
-interface LiveLayoutProps extends HTMLAttributes<HTMLDivElement> {
-    children?: React.ReactNode
-}
-
-export default function LiveLayout({ children }: LiveLayoutProps) {
+export default function LiveLayout({ children }: { children: React.ReactNode }) {
     return <div className="w-full flex items-center justify-center mt-[12px] p-2">
         <div
             className="flex flex-col items-center justify-center w-full p-2 h-max rounded-[8px] shadow-2xl md:w-[80%] md:p-4 md:shadow-xl"
