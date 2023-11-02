72cabc44c207aea9e3d6c8b05233dbefda87822a
Henry Le <lehieu2077@gmail.com>
2023-11-02 22:46:34 +0700

    feat: add location api for map view

---
+++ src/apis/constants/map.ts
@@ -0,0 +1,13 @@
+export const UTRAFFIC_LOCATION_FETCHING_HEADER = {
+    Host: "discover.search.hereapi.com",
+    Origin: "https://bktraffic.com",
+    Referer: "https://bktraffic.com/",
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/117.0.0.0 Safari/537.36",
+}
+
+/**
+ * query: at=lat,lng&limit=limit&apikey=apikey&q=place
+ */
+export const UTRAFFIC_LOCATION_FETCHING_URL = "https://discover.search.hereapi.com/v1/discover?"
+export const UTRAFFIC_API_KEY = "x7baztljil7z6LqHGkeoxoKU_FpllTZtUeNwlB_BdUk"
+export const UTRAFFIC_AT_COORD = "10.7725104,106.6575988"
---
+++ src/apis/map.ts
@@ -0,0 +1,45 @@
+import axios from "axios"
+import { UTRAFFIC_API_KEY, UTRAFFIC_AT_COORD, UTRAFFIC_LOCATION_FETCHING_URL } from "./constants/map"
+import { IBKLocationItemBrief, IBKLocationOptions, IBKLocationSearchDataResponse, IBKLocationSearchParameters } from "./interfaces"
+
+export const fetchLocationOptions = async (
+    {
+        latitude,
+        longitude,
+        limit = "5",
+        location
+    }: IBKLocationSearchParameters
+): Promise<IBKLocationOptions | undefined> => {
+    try {
+        const url = UTRAFFIC_LOCATION_FETCHING_URL
+
+        const at = latitude && longitude ? `${latitude},${longitude}` : UTRAFFIC_AT_COORD;
+        const q = location;
+        const apikey = UTRAFFIC_API_KEY;
+
+        const endpoint = `${url}at=${at}&limit=${limit}&apikey=${apikey}&q=${q}`
+        const res = await axios.get(endpoint)
+
+        if (res.status != 200) {
+            throw new Error("Error when fetching location options")
+        }
+
+        const data: IBKLocationSearchDataResponse = res.data
+
+        const items = data.items.reduce((acc, item) => {
+            const { title, position } = item
+
+            if (!title || !position) return acc;
+
+            return [...acc, { title, position }]
+
+        }, [] as IBKLocationItemBrief[])
+
+        return {
+            items
+        }
+    }
+    catch (err) {
+        console.log(err)
+    }
+}
