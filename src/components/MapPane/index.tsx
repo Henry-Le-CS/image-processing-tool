a831534123743d0bc298fe86802dd02ac4fd0141
Henry Le <lehieu2077@gmail.com>
2023-11-02 23:35:30 +0700

    update: add selected camera id state for map pane

--- src/components/MapPane/index.tsx
+++ src/components/MapPane/index.tsx
@@ -8,15 +8,22 @@ import MapView from './MapView';
 
 export default function MapPane() {
   const [camerasInRange, setCamerasInRange] = useState<ICameraData[]>([]);
+  const [selectedCameraId, setSelectedCameraId] = useState<string>('');
 
   return (
     <>
       <Row className="w-full md:min-w-[90vw] flex gap-2">
         <Col flex={2} className="rounded border p-2 max-w-[40%]">
-          <CameraStatistic cameras={camerasInRange} />
+          <CameraStatistic
+            selectedCameraId={selectedCameraId}
+            cameras={camerasInRange}
+            setSelectedCameraId={setSelectedCameraId}
+          />
         </Col>
         <Col flex={3} className="rounded border p-2">
           <MapView
+            selectedCameraId={selectedCameraId}
+            setSelectedCameraId={setSelectedCameraId}
             camerasInRange={camerasInRange}
             setCamerasInRange={setCamerasInRange}
           />
