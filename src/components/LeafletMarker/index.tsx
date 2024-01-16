51980ac6494268cf326dacc34bbb75535babc845
hitocnichi <42115291+hitonichi@users.noreply.github.com>
2024-01-16 17:44:49 +0700

    feat: add camera marker

--- src/components/LeafletMarker/index.tsx
+++ src/components/LeafletMarker/index.tsx
@@ -5,17 +5,35 @@ import { Marker as LeafletMarker } from 'react-leaflet';
 const MARKER_ICON = icon({
   iconUrl: '/marker-icon.png',
   iconSize: [32, 48],
+  iconAnchor: [16, 48],
+});
+
+const CAMERA_ICON = icon({
+  iconUrl: '/camera-marker-icon.png',
+  iconSize: [48, 48],
+  iconAnchor: [24, 48],
 });
 
 export default function Marker({
+  type = 'default',
   position,
   children,
 }: {
+  type?: string;
   position: LatLngExpression;
   children: ReactNode;
 }) {
+  let icon;
+  switch (type) {
+    case 'camera':
+      icon = CAMERA_ICON;
+      break;
+    default:
+      icon = MARKER_ICON;
+      break;
+  }
   return (
-    <LeafletMarker icon={MARKER_ICON} position={position}>
+    <LeafletMarker icon={icon} position={position}>
       {children}
     </LeafletMarker>
   );
